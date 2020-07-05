af43739dabce2971c42ae91348758972613ec1cb
artiq <art.iq@hotmail.com>
2020-07-06 00:59:05 +0300

    протестировано добавление записей. нужно проверить кодировки

--- DB/Program.cs
+++ DB/Program.cs
@@ -10,6 +10,28 @@ namespace DB
             string path = @"data.csv";
             Repository db = new Repository(path);
             Console.WriteLine($"Файл {path} открыт успешно.");
+
+            Console.WriteLine("Введите новую запись:");
+            Console.Write("Дата операции (dd.mm.yyyy):");
+            DateTime newDate = Convert.ToDateTime(Console.ReadLine());
+
+            Console.Write("Вид операции (доход: 1 / расход: -1):");
+            sbyte newType = Convert.ToSByte(Console.ReadLine());
+
+            Console.Write("Сумма:");
+            double newSum = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Счет:");
+            string newAcc = (Console.ReadLine());
+
+            Console.Write("Категория дохода/расхода:");
+            string newCat = (Console.ReadLine());
+
+            Console.Write("Примечание:");
+            string newNot = (Console.ReadLine());
+
+            db.Add(new Record(newDate, newType, newSum, @newAcc, @newCat, @newNot)); 
+
             db.Save();
             Console.WriteLine($"База выгружена. Файл {path} записан успешно.");
             Console.ReadKey();
