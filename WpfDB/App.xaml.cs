0cc6d92f5622fecf4e8c5e146f3ef58628a68a5d
artiq <art.iq@hotmail.com>
2020-07-23 20:41:06 +0300

    чтение/формирование файла настроек добавлено в App.xaml.cs

--- WpfDB/App.xaml.cs
+++ WpfDB/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -14,7 +15,30 @@ namespace WpfDB
     /// </summary>
     public partial class App : Application
     {
-       
-        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            string iniPath = @"settings.ini";
+            
+            iniValues Settings;
+            if (File.Exists(iniPath))
+            { 
+                Settings = new iniValues(@"settings.ini");
+            }
+
+            else
+            {
+                DateTime date = Convert.ToDateTime("02.01.2020");
+                double balance = 9999.99;
+                string accounts = "карта, наличные, кредит";
+                string inCats = "зарплата, подработка, проценты, благотворительность, воровство, подарки/находки, кредиты, прочее";
+                string outCats = "продукты, дом, коммунальные, животные, отдых, кредиты, инвестиции, одежда, прочее";
+                Settings = new iniValues(date, balance, accounts, inCats, outCats);
+                Settings.Save(@"settings.ini");
+                MessageBox.Show($@"Сформирован файл начальных настроек учета по адресу: {Directory.GetCurrentDirectory ()}\{iniPath}");
+                
+            }
+
+        }
+
     }
 }
